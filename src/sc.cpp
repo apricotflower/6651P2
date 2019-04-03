eb01801bf39a05b6ec79957fbc9060d0fe5a5be5
liang <liangliang44@hotmail.com>
2019-04-03 18:51:11 -0400

    增加判断

--- src/sc.cpp
+++ src/sc.cpp
@@ -9,67 +9,64 @@ struct pixel{
     float initialE = 0;
     float optEnergy = 0;
     int optPreColIndex;
-//    pixel *optPreLoc;
+
 };
 
-void delSeam(Mat& tempMat, Mat& new_image, pixel **curMat){//把最小seam所在pixel寻找到并删除，图片缩小存入new_image
-    int rows = tempMat.rows;
-    int cols = tempMat.cols;
+
+
+void delSeam(Mat &tempMat, Mat &new_image, pixel **curMat, int rows, int &tempMin){//找出能量最小值的col并删除
     int lastRowIndex = rows-1;
 
-    int tempMin = 0;
-    float tempMinEnergy = curMat[lastRowIndex][0].optEnergy;
-    for (int j = 1; j < cols; j++) {
-        if(curMat[lastRowIndex][j].optEnergy < tempMinEnergy){
-            tempMin = j;
-            tempMinEnergy = curMat[lastRowIndex][j].optEnergy;
-        }
-    }
-
     int curColIndex = tempMin;
-//    pixel *curloc = &curMat[lastRowIndex][tempMin];
-
-    for (int j = 0; j < new_image.cols; j++) {//最后一行
+    for (int j = 0; j < new_image.cols; j++) {
         if(j < curColIndex){
-            new_image.at<Vec3b>(lastRowIndex, j) = tempMat.at<Vec3b>(lastRowIndex, j);
+            new_image.at<Vec3b>(lastRowIndex, j) = tempMat.at<Vec3b>(lastRowIndex, j);;
         }else{
             new_image.at<Vec3b>(lastRowIndex, j) = tempMat.at<Vec3b>(lastRowIndex, j + 1);
         }
     }
 
-    for (int i = lastRowIndex; i > 0; i--) {//最后一行以外
+    int rowIndex = lastRowIndex;
+    while (rowIndex != 0) {
+
+        curColIndex = curMat[rowIndex][curColIndex].optPreColIndex;
         for (int j = 0; j < new_image.cols; j++) {
-            curColIndex = curMat[i][curColIndex].optPreColIndex;
-            if(j < curColIndex){
-                new_image.at<Vec3b>(i-1, j) = tempMat.at<Vec3b>(i-1, j);
-            }else{
-                new_image.at<Vec3b>(i-1, j) = tempMat.at<Vec3b>(i-1, j + 1);
+            if (j < curColIndex) {
+                new_image.at<Vec3b>(rowIndex-1, j) = tempMat.at<Vec3b>(rowIndex-1, j);
+            } else {
+                new_image.at<Vec3b>(rowIndex-1, j) = tempMat.at<Vec3b>(rowIndex-1, j + 1);
             }
         }
+        rowIndex--;
+
     }
 
 }
 
-void optCalLeft(int& i, int& j , pixel **curMat){
+void optCalLeft(int i, int j , pixel **curMat){
     curMat[i][j].optEnergy = curMat[i-1][j-1].optEnergy + curMat[i][j].initialE;
     curMat[i][j].optPreColIndex = j - 1;
-//    curMat[i][j].optPreLoc = &curMat[i-1][j-1];
+
 }
 
-void optCalRight(int& i, int& j , pixel **curMat){
+void optCalRight(int i, int j , pixel **curMat){
     curMat[i][j].optEnergy = curMat[i-1][j+1].optEnergy + curMat[i][j].initialE;
     curMat[i][j].optPreColIndex = j + 1;
-//    curMat[i][j].optPreLoc = &curMat[i-1][j+1];
+
 }
 
-void optCalMid(int& i, int& j , pixel **curMat){
+void optCalMid(int i, int j , pixel **curMat){
     curMat[i][j].optEnergy = curMat[i-1][j].optEnergy + curMat[i][j].initialE;
     curMat[i][j].optPreColIndex = j;
-//    curMat[i][j].optPreLoc = &curMat[i-1][j];
+
 }
 
+
 //能量方程公式 E= sqrt((up-down)^2 + (left-right)^2)
-void calOptEnergy(Mat& tempMat,pixel **curMat,int& rows,int& cols){//计算每pixel能量最小值
+void calOptEnergy(Mat& tempMat,pixel **curMat,int& rows,int& cols, int& tempMin){//计算每pixel能量最小值
+
+    float tempMinEnergy;
+
     int lastCol = cols - 1;
     int lastRow= rows - 1;
 
@@ -147,47 +144,64 @@ void calOptEnergy(Mat& tempMat,pixel **curMat,int& rows,int& cols){//计算每pi
 
     }
 
-    for(int j = 0; j < cols; j++){//最后一行
-        if(j == 0){
-            curMat[lastRow][0].initialE = (float)sqrt(pow(((float)tempGrayMat.at<uchar>(lastRow,1)),2) + pow(((float)tempGrayMat.at<uchar>(lastRow - 1,0)),2));
+    for(int j = 0; j < cols; j++) {//最后一行
+        if (j == 0) {
+            curMat[lastRow][0].initialE = (float) sqrt(pow(((float) tempGrayMat.at<uchar>(lastRow, 1)), 2) +
+                                                       pow(((float) tempGrayMat.at<uchar>(lastRow - 1, 0)), 2));
 
-            if(curMat[lastRow-1][j].optEnergy > curMat[lastRow-1][j+1].optEnergy){//右边
+            if (curMat[lastRow - 1][j].optEnergy > curMat[lastRow - 1][j + 1].optEnergy) {//右边
 
-                optCalRight(lastRow,j,curMat);
+                optCalRight(lastRow, j, curMat);
 
-            }else{//中间
+            } else {//中间
 
-                optCalMid(lastRow,j,curMat);
+                optCalMid(lastRow, j, curMat);
             }
 
-        }else if (j == lastCol){
-            curMat[lastRow][lastCol].initialE = (float)sqrt(pow(((float)tempGrayMat.at<uchar>(lastRow,lastCol - 1)),2) + pow(((float)tempGrayMat.at<uchar>(lastRow - 1,lastCol)),2));
+            tempMinEnergy = curMat[lastRow][0].optEnergy;
 
-            if(curMat[lastRow-1][j].optEnergy > curMat[lastRow-1][j-1].optEnergy){//左边
+        } else {
+            if (j == lastCol) {
+                curMat[lastRow][lastCol].initialE = (float) sqrt(
+                    pow(((float) tempGrayMat.at<uchar>(lastRow, lastCol - 1)), 2) +
+                    pow(((float) tempGrayMat.at<uchar>(lastRow - 1, lastCol)), 2));
 
-                optCalLeft(lastRow,j,curMat);
+                if (curMat[lastRow - 1][j].optEnergy > curMat[lastRow - 1][j - 1].optEnergy) {//左边
 
-            }else{//中间
+                    optCalLeft(lastRow, j, curMat);
 
-                optCalMid(lastRow,j,curMat);
-            }
+                } else {//中间
 
-        }else{
+                    optCalMid(lastRow, j, curMat);
+                }
 
-            curMat[tempGrayMat.rows-1][j].initialE = (float)sqrt(pow(((float)tempGrayMat.at<uchar>(tempGrayMat.rows-1,j-1) - (float)tempGrayMat.at<uchar>(tempGrayMat.rows-1,j+1)),2) + pow(((float)tempGrayMat.at<uchar>(tempGrayMat.rows-2,j)),2));
+            } else {
 
-            float minEnergy = curMat[lastRow-1][j].optEnergy;//中间
-            optCalMid(lastRow,j,curMat);
+                curMat[tempGrayMat.rows - 1][j].initialE = (float) sqrt(
+                    pow(((float) tempGrayMat.at<uchar>(tempGrayMat.rows - 1, j - 1) -
+                         (float) tempGrayMat.at<uchar>(tempGrayMat.rows - 1, j + 1)), 2) +
+                    pow(((float) tempGrayMat.at<uchar>(tempGrayMat.rows - 2, j)), 2));
 
-            if(minEnergy > curMat[lastRow-1][j-1].optEnergy){//左边
-                minEnergy = curMat[lastRow-1][j-1].optEnergy;
-                optCalLeft(lastRow,j,curMat);
-            }
+                float minEnergy = curMat[lastRow - 1][j].optEnergy;//中间
+                optCalMid(lastRow, j, curMat);
 
-            if(minEnergy > curMat[lastRow-1][j+1].optEnergy){//右边
-                optCalRight(lastRow,j,curMat);
+                if (minEnergy > curMat[lastRow - 1][j - 1].optEnergy) {//左边
+                    minEnergy = curMat[lastRow - 1][j - 1].optEnergy;
+                    optCalLeft(lastRow, j, curMat);
+                }
+
+                if (minEnergy > curMat[lastRow - 1][j + 1].optEnergy) {//右边
+                    optCalRight(lastRow, j, curMat);
+                }
             }
         }
+
+
+        if(curMat[lastRow][j].optEnergy < tempMinEnergy){
+            tempMin = j;
+            tempMinEnergy = curMat[lastRow][j].optEnergy;
+        }
+
     }
 
 }
@@ -203,16 +217,20 @@ void operateOneSeam(Mat& tempMat){//删除一条seam操作
         curMat[i] = new pixel [cols]  ;
     }
 
-
-    calOptEnergy(tempMat , curMat , rows,cols);
+    int tempMin = 0;
+    calOptEnergy(tempMat , curMat , rows,cols,tempMin);
 
     Mat new_image(rows,cols-1,tempMat.type());
-    delSeam(tempMat, new_image, curMat);
+
+
+    delSeam(tempMat, new_image, curMat, rows, tempMin);
+
 
     for( int i=0; i < rows; i++ ){
         delete[] curMat[i];
     }
     delete[] curMat;
+
 
     tempMat = new_image;
 
@@ -247,17 +265,14 @@ bool seam_carving(Mat& in_image, int new_width, int new_height, Mat& out_image){
     return seam_carving_trivial(in_image, new_width, new_height, out_image);
 }
 
-
-// seam carves by removing trivial seams
-bool seam_carving_trivial(Mat& in_image, int new_width, int new_height, Mat& out_image){
-
-    Mat tempMat = in_image.clone();
-
+void seam_carving_v(Mat& tempMat, int new_width){
     while (tempMat.cols > new_width) {
 //        cout<<"v: "<<tempMat.cols <<endl;
         operateOneSeam(tempMat);
     }
+}
 
+void seam_carving_h(Mat& tempMat, int new_height){
     //旋转图片tempMat，切横边
     transpose(tempMat,tempMat);
     flip(tempMat,tempMat,1);
@@ -270,7 +285,20 @@ bool seam_carving_trivial(Mat& in_image, int new_width, int new_height, Mat& out
     //再次旋转图片,恢复
     transpose(tempMat,tempMat);
     flip(tempMat,tempMat,0);
+}
 
+// seam carves by removing trivial seams
+bool seam_carving_trivial(Mat& in_image, int new_width, int new_height, Mat& out_image){
+
+    Mat tempMat = in_image.clone();
+
+    if(in_image.cols > in_image.rows){//判断，先切比较长的方向
+        seam_carving_h(tempMat,new_height);
+        seam_carving_v(tempMat,new_width);
+    }else{
+        seam_carving_v(tempMat,new_width);
+        seam_carving_h(tempMat,new_height);
+    }
     out_image = tempMat;
 
     return true;
